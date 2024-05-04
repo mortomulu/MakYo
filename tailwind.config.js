60d377ec4a7bb3ff5c2473d66cc569a5a17aff84
mortomulu <mfajrihidayat09@gmail.com>
2024-05-04 14:14:11 +0700

    install n config tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],  theme: {
+    fontFamily: {
+      'magra': '"Magra"',
+      'linden-hill' : '"Linden Hill"',
+      'mulish' : '"Mulish"',
+      'noto-serif' : '"Noto-Serif',
+      'lexend-deca' : '"Lexend Deca"'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
