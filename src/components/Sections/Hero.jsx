127b41d09370825f4b12eb67ff8ae3c3c67abb88
mortomulu <mfajrihidayat09@gmail.com>
2024-05-04 18:45:13 +0700

    fix: bug width page

--- src/components/Sections/Hero.jsx
+++ src/components/Sections/Hero.jsx
@@ -5,10 +5,10 @@ import poster from "../../assets/poster.png";
 
 const Hero = () => {
   return (
-    <div className="w-[1440px] h-[1092px] bg-main">
+    <div className="w-full h-[1092px] bg-main">
       <Navbar />
       <Logo />
-      <div className="flex pl-28 pt-20 ">
+      <div className="flex pl-28 pt-20 pr-20 ">
         <HeroContent />
         <img className="w-[618px] h-[854px] mt-[-80px] ml-10" src={poster} alt="" />
       </div>
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,7 +2,7 @@ import Hero from "../components/Sections/Hero";
 
 export default function LandingPage() {
   return (
-    <div className="w-[1440px]">
+    <div className="w-full">
       <Hero />
     </div>
   );
